9560707872afe811b933f08ae0384260dc027ef9
nahjh2 <nahjh2@google.com>
2020-09-18 23:09:36 +0900

    account page UI

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatefulWidget {
@@ -10,6 +11,7 @@ class _AccountPageState extends State<AccountPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
+      body: _buildBody(),
     );
   }
 
@@ -21,6 +23,77 @@ class _AccountPageState extends State<AccountPage> {
           onPressed: () {},
         )
       ],
+    );
+  }
+
+  _buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://img.brickowl.com/files/image_cache/larger/lego-universe-bob-minifigure-25.jpg"),
+                    ),
+                  ),
+                  Container(
+                    width: 80,
+                    height: 80,
+                    alignment: Alignment.bottomRight,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          width: 28,
+                          height: 28,
+                          child: FloatingActionButton(
+                            onPressed: null,
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 25,
+                          height: 25,
+                          child: FloatingActionButton(
+                            onPressed: null,
+                            backgroundColor: Colors.blue,
+                            child: Icon(Icons.add),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+              ),
+              Text(
+                "이름",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              )
+            ],
+          ),
+          Text(
+            "0\n게시물",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18),
+          ),
+          Text("0\n팔로워",
+              textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+          Text("0\n팔로잉",
+              textAlign: TextAlign.center, style: TextStyle(fontSize: 18)),
+        ],
+      ),
     );
   }
 }
