9d2315e935076001062ff14a7809de56caed76cc
nahjh2 <nahjh2@google.com>
2020-09-19 23:25:57 +0900

    add create page

--- lib/create_page.dart
+++ lib/create_page.dart
@@ -6,10 +6,25 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
+  final textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    // 소멸될때 실행
+    // TODO: implement dispose
+    textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // 생성될때 실행
     return Scaffold(
       appBar: _buildAppBar(),
+      body: _buildBody(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_a_photo),
+      ),
     );
   }
 
@@ -17,7 +32,20 @@ class _CreatePageState extends State<CreatePage> {
     return AppBar(
       actions: [
         IconButton(
-            icon: Icon(Icons.send)
+          icon: Icon(Icons.send),
+          onPressed: () {},
+        )
+      ],
+    );
+  }
+
+  _buildBody() {
+    return Column(
+      children: [
+        Text("No Image"),
+        TextField(
+          decoration: InputDecoration(hintText: "내용을 입력하세요"),
+          controller: textEditingController,
         )
       ],
     );
