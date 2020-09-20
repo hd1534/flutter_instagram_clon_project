2d8bba3532371bda82d5484c972809dfc7435255
nahjh2 <nahjh2@google.com>
2020-09-20 18:58:24 +0900

    Sign In Button (google)

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("LoginPage")
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Instagram Clon",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),),
+
+            Padding(padding: EdgeInsets.all(50),),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:instagramclon/login_page.dart';
 import 'package:instagramclon/tab_page.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
   }
 }
