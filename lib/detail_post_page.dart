fc16c39e39f089e2671cae7d8000d666b27eeb7f
nahjh2 <nahjh2@google.com>
2020-09-24 14:29:52 +0900

    detail page

--- lib/detail_post_page.dart
+++ lib/detail_post_page.dart
@@ -1,8 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DetailPostPage extends StatelessWidget {
   final dynamic document;
-
 
   DetailPostPage(this.document);
 
@@ -12,7 +12,6 @@ class DetailPostPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("둘러보기"),
       ),
-
       body: _buildBody(),
     );
   }
@@ -23,20 +22,37 @@ class DetailPostPage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(padding: EdgeInsets.all(8),
+              Padding(
+                padding: EdgeInsets.all(8),
                 child: Row(
                   children: [
                     CircleAvatar(
-                      backgroundImage: NetworkImage(
-                          document.get('writer')['photoURL']),
+                      backgroundImage:
+                      NetworkImage(document.get('writer')['photoURL']),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            document.get('writer')['email'],
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Text(document.get('writer')['name'])
+                        ],
+                      ),
                     )
                   ],
-                ),),
-
-
+                ),
+              ),
+              Image.network(document.get('photoURL')),
+              Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(document.get('contents'))
+              )
             ],
           ),
-        )
-    );
+        ));
   }
 }
